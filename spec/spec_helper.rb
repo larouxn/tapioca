babe6ab44d91dff970f4fb15355190df8437551a
Nicholas La Roux <larouxn@gmail.com>
2026-01-26 15:30:14 -0500

    Clean up minitest usage now that minitest 6 support is widely supported

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,23 +5,17 @@ require "tapioca/internal"
 require "minitest/autorun"
 require "minitest/spec"
 require "minitest/hooks/default"
+require "minitest/reporters"
+require "minitest/mock"
 require "rails/test_unit/line_filtering"
 
 require "tapioca/helpers/test/content"
 require "tapioca/helpers/test/template"
 require "tapioca/helpers/test/isolation"
+require "spec_reporter"
 require "dsl_spec_helper"
 require "spec_with_project"
 require "rails_spec_helper"
-
-require "minitest/reporters"
-require "spec_reporter"
-
-# Minitest::Reporters currently lacks support for Minitest 6 out of the box
-# but we can register the plugin to use it.
-# Ref: https://github.com/minitest-reporters/minitest-reporters/pull/366#issuecomment-3731951673
-require "minitest/minitest_reporter_plugin"
-Minitest.register_plugin(:minitest_reporter)
 
 backtrace_filter = Minitest::ExtensibleBacktraceFilter.default_filter
 backtrace_filter.add_filter(%r{gems/sorbet-runtime})
@@ -29,8 +23,6 @@ backtrace_filter.add_filter(%r{gems/railties})
 backtrace_filter.add_filter(%r{tapioca/helpers/test/})
 
 Minitest::Reporters.use!(SpecReporter.new(color: true), ENV, backtrace_filter)
-
-require "minitest/mock"
 
 module Minitest
   class Test
